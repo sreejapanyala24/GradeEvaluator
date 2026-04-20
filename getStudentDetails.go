7e5348b206bc4c37a3575c3689eab74fcd91b7e1
sreejapanyala24 <sreejapanyala24@gmail.com>
2026-04-20 10:38:29 -0500

    updated student details

--- getStudentDetails.go
+++ getStudentDetails.go
@@ -10,7 +10,7 @@ func getStudentDetails() []student {
 	students := []student{
 		{1, "Sarayu", 94},
 		{2, "Priya", 77},
-		{3, "Arjun", 64},
+		{3, "Arjun", 68},
 		{4, "Vikram", 85},
 		{5, "Shyam", 60},
 		{6, "Teja", 98},
