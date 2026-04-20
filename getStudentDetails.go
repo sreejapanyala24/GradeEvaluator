5d222a8cf4361b6dc46934db9893cbfe32fa23f1
sreejapanyala24 <sreejapanyala24@gmail.com>
2026-04-20 10:54:29 -0500

    updated student details

--- getStudentDetails.go
+++ getStudentDetails.go
@@ -9,7 +9,7 @@ type student struct {
 func getStudentDetails() []student {
 	students := []student{
 		{1, "Sarayu", 94},
-		{2, "Priya", 78},
+		{2, "Priya", 75},
 		{3, "Arjun", 68},
 		{4, "Vikram", 85},
 		{5, "Shyam", 60},
